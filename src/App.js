6747b87f7f1ceec24d9a1a9800424f9aba2ae1f6
Sam-BCC <65135395+Sam-BCC@users.noreply.github.com>
2020-06-09 05:36:32 +0300

    App updated 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import Header from './Header';
-import MemeGenerator from './MemeGenerator';
-function App() {
-  return (
-    <div>
-      <Header />
-      <MemeGenerator />
-    </div>
-  );
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from "./header"
+import MemeGenerator from "./MemeGenerator"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header 
+          name="Meme Generator"
+        />
+        <MemeGenerator 
+        />
+      </div>
+    );
+  }
 }
+
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,18 @@
-import React from 'react';
-function Header() {
-  return <h1>HEADER</h1>;
+  
+import React from "react"
+
+class Header extends React.Component {
+    render(){
+        return(
+            <header>
+                <img 
+                    src = "http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+                    alt="problem ?"
+                />
+                <p>{this.props.name}</p>
+            </header>
+        )
+    }
 }
-export default Header;
+
+export default Header
